fa036dad161a46835e4a721ff3745e12ead2268e
ashishbhartii <109731651+ashishbhartii@users.noreply.github.com>
2024-06-04 11:51:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let year = window.promt("Enter a Year");
+let result = daysOfYear(year);
+console.log(result);
+function daysOfYear(year){
+	if (year % 4 == 0){
+		return 366;
+	}
+	else{
+		return 365;
+	}
+}
