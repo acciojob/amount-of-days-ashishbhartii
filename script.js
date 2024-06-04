e840e9d099c15d91b4ffb1980d1e6a2baee5362d
ashishbhartii <109731651+ashishbhartii@users.noreply.github.com>
2024-06-04 12:29:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,14 +3,10 @@ let year = window.promt("Enter a Year");
 let result = daysOfYear(year);
 console.log(result);
 function daysOfYear(year){
-	if (year % 100 === 0){
-		if(year % 400 === 0){
-			if(year % 4 === 0){
-				return 366;
-			}
-		}
+	if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0){
+		return 366;
 	}
 	else{
-			return 365;
-		}
+		return 365;
+	}
 }
